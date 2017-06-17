109c1d7ccac4765ba544ebb01e63584591339f8c
TJ Sartain <troy.sartain@penske.com>
2017-06-17 18:36:38 -0400

    Added taps for info button

--- AmericanFlag/ViewController.swift
+++ AmericanFlag/ViewController.swift
@@ -10,6 +10,25 @@ import UIKit
 
 class ViewController: UIViewController
 {
+    
+    @IBOutlet weak var infoButton: UIButton!
+    
+    @IBAction func tap(_ sender: Any)
+    {
+        fadeButton(show: self.infoButton.alpha == 0)
+    }
+    
+    func fadeButton(show: Bool)
+    {
+        UIView.animate(withDuration: 0.5,
+                       delay: 0,
+                       options: [.curveEaseIn],
+                       animations:
+                        {
+                            self.infoButton.alpha = show ? 1 : 0
+                        },
+                       completion: nil)
+    }
 
 }
 
